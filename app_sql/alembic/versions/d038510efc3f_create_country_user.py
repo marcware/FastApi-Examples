4a0714c70229c1646d7238f70906c020e87437f6
Marcware <fcoparo@gmail.com>
2023-08-17 22:19:31 +0200

    Second commit

---
+++ app_sql/alembic/versions/d038510efc3f_create_country_user.py
@@ -0,0 +1,26 @@
+"""create country user
+
+Revision ID: d038510efc3f
+Revises: 
+Create Date: 2023-08-10 22:05:06.056830
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd038510efc3f'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
